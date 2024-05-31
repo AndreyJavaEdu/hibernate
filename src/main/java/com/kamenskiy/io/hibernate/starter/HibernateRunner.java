ccbaa64f558f8b0ea16612f9cf6f7a16172c7de6
AndreyJavaEdu <scream123456@yandex.ru>
2024-05-31 21:50:39 +0500

    Реализовали entity класс User, проаннотировали его, и выполнили в транзакции метод persist() по добавлению юзера в БД.

--- src/main/java/com/kamenskiy/io/hibernate/starter/HibernateRunner.java
+++ src/main/java/com/kamenskiy/io/hibernate/starter/HibernateRunner.java
@@ -1,4 +1,29 @@
 package com.kamenskiy.io.hibernate.starter;
+import com.kamenskiy.io.hibernate.entity.User;
+import org.hibernate.cfg.Configuration;
+
+import java.time.LocalDate;
 
 public class HibernateRunner {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+//        configuration.addAnnotatedClass(User.class);
+//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+
+        try (var sesssionFactory = configuration.buildSessionFactory();
+             var session = sesssionFactory.openSession();) {
+
+            session.beginTransaction();
+
+            session.persist(User.builder()
+                            .username("kamenskiy@gmail.com")
+                            .firstname("Andrey")
+                            .lastname("Kamenskiy")
+                            .birthDate(LocalDate.of(1990, 1, 23))
+                            .age(34)
+                    .build());
+            session.getTransaction().commit();
+        }
+    }
 }
