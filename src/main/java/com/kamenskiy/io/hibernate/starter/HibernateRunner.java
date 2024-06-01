d50831f9cdbefe77fba1a57f7f3cfbf95f88fe52
AndreyJavaEdu <scream123456@yandex.ru>
2024-06-01 19:51:56 +0500

    Разбора метода isDirty().

--- src/main/java/com/kamenskiy/io/hibernate/starter/HibernateRunner.java
+++ src/main/java/com/kamenskiy/io/hibernate/starter/HibernateRunner.java
@@ -1,43 +1,34 @@
 package com.kamenskiy.io.hibernate.starter;
 
-import com.kamenskiy.io.hibernate.converter.BirthdayConverter;
 import com.kamenskiy.io.hibernate.entity.Birthday;
 import com.kamenskiy.io.hibernate.entity.Role;
 import com.kamenskiy.io.hibernate.entity.User;
-import org.hibernate.cfg.Configuration;
+import com.kamenskiy.io.hibernate.util.HibernateUtil;
 
 import java.time.LocalDate;
 
 public class HibernateRunner {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration();
-        configuration.configure("hibernate.cfg.xml");
-        configuration.addAttributeConverter(BirthdayConverter.class, true);
-//        configuration.addAnnotatedClass(User.class);
-//        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
+        //TRANSIENT
+        var user = User.builder()
+                .username("kamenskiy2@gmail.com")
+                .firstname("Andrey")
+                .lastname("Kamenskiy")
+                .birthday(new Birthday(LocalDate.of(1990, 2, 12)))
+                .role(Role.USER)
+                .build();
 
-        try (var sesssionFactory = configuration.buildSessionFactory();
-             var session = sesssionFactory.openSession();) {
+        //TRANSIENT
+        try (var sesssionFactory = HibernateUtil.buildSessionFactory();) {
+            try (var session1 = sesssionFactory.openSession()) {
 
-            session.beginTransaction();
-            var user = User.builder()
-                    .username("kamenskiy2@gmail.com")
-                    .firstname("Andrey")
-                    .lastname("Kamenskiy")
-                    .birthday(new Birthday(LocalDate.of(1990, 2, 12)))
-                    .role(Role.USER)
-                    .build();
-//            session.persist(user);
-//            session.update(user);
-//            session.saveOrUpdate(user);
-//            session.delete(user);
-            User user1 = session.get(User.class, "kamenskiy2@gmail.com");
-            user1.setFirstname("Ivan");
-//            session.evict(user1);
-
-            System.out.println(user1);
-
-            session.getTransaction().commit();
+                session1.beginTransaction();
+                //user PERSISTENT к сессии 1
+                session1.saveOrUpdate(user);
+                user.setFirstname("Olga");
+                System.out.println(session1.isDirty());
+                session1.getTransaction().commit();
+            }
         }
     }
 }
