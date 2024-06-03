1287f18bcd3912fb619abd76798809e8db5215e3
AndreyJavaEdu <scream123456@yandex.ru>
2024-06-03 18:42:15 +0500

    @OneToMany. Проблемы с both-side маппингом. Добавление сущностей в связанны таблицах. OrphalRemomal.

--- src/main/java/com/kamenskiy/io/hibernate/entity/User.java
+++ src/main/java/com/kamenskiy/io/hibernate/entity/User.java
@@ -1,26 +1,34 @@
 package com.kamenskiy.io.hibernate.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-
+@ToString(exclude = "company")
+@EqualsAndHashCode(of = "username")
 @Entity
 @Table(name = "users", schema = "public")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
+@Column(unique = true, nullable = false)
     private String username;
     @Embedded
     private PersonalInfo personalInfo;
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id")
+    private Company company;
+
+    protected boolean canEqual(final Object other) {
+        return other instanceof User;
+    }
+
 }
--- src/main/java/com/kamenskiy/io/hibernate/util/HibernateUtil.java
+++ src/main/java/com/kamenskiy/io/hibernate/util/HibernateUtil.java
@@ -1,6 +1,8 @@
 package com.kamenskiy.io.hibernate.util;
 
 import com.kamenskiy.io.hibernate.converter.BirthdayConverter;
+import com.kamenskiy.io.hibernate.entity.Birthday;
+import com.kamenskiy.io.hibernate.entity.Company;
 import com.kamenskiy.io.hibernate.entity.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -10,6 +12,7 @@ public class HibernateUtil {
         Configuration configuration = new Configuration();
         configuration.configure("hibernate.cfg.xml");
         configuration.addAnnotatedClass(User.class);
+        configuration.addAnnotatedClass(Company.class);
         configuration.addAttributeConverter(BirthdayConverter.class, true);
         //        configuration.addAnnotatedClass(User.class);
         //        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
